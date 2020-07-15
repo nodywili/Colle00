1521bafcaf5d3f9908d5a232ee7774ff8bfb6fa5
Nompilo Dywili <nodywili@c4r7s5.wethinkcode.co.za>
2020-07-15 16:10:35 +0200

    Rushes

---
+++ ex00/rush02.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nodywili <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 10:28:10 by nodywili          #+#    #+#             */
+/*   Updated: 2020/07/15 16:06:32 by nodywili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void ft_putchar(char c);
+
+void rush(int x, int y){
+		
+				int row;
+				int column;
+
+				 row = 0;
+				 column = 0;
+
+				while(row < y ){
+					while(column < x){
+								 if((column == 0  || row == (x -1)) && (column == (x-1) || row == (y -1)))
+								 {
+								 				ft_putchar('c');
+								 }
+								 else if(( row == 0 || column == 0) && (row == 0 || column == (x-1)))
+								 {
+								 				ft_putchar('A');
+								 }
+								 else if(row == 0 || row ==  (y -1))
+								 {
+								 				ft_putchar('B');
+											
+								 }
+								 else if(column == 0 || column ==  (x-1))
+								 {
+								 				ft_putchar('B');
+											
+								 }
+								 else
+								 {
+								 			ft_putchar(' ');
+								 }
+								 column++;
+								 
+				}
+					column = 0;
+					ft_putchar('\n');
+					row++;
+}
+}
